73b0b82d89c33c6f16c50160df0528d9001e1314
gabrineno <gabrineno@gabrineno-300E4A-300E5A-300E7A-3430EA-3530EA>
2016-02-05 10:35:19 +0100

    Creacion de la base de datos MySql

--- proyectoMVCgam/BaseDatos/Script.sql
+++ proyectoMVCgam/BaseDatos/Script.sql
@@ -4,42 +4,49 @@ USE Clase;
 
 DROP TABLE IF EXISTS Alumnos;
 CREATE TABLE IF NOT EXISTS Alumnos(
-dni VARCHAR(10) PRIMARY KEY,
+dni VARCHAR(9) PRIMARY KEY,
 nombre VARCHAR(255) NOT NULL,
 apellidos VARCHAR(255) NOT NULL,
-edad INTEGER(2) NOT NULL,
-profesor VARCHAR(10)
-);
-
-DROP TABLE IF EXISTS Profesores;
-CREATE TABLE IF NOT EXISTS Profesores(
-dni VARCHAR(10) PRIMARY KEY,
-nombre VARCHAR(255) NOT NULL,
-apellidos VARCHAR(255) NOT NULL,
-asignatura VARCHAR(30) NOT NULL
+edad INTEGER(2) NOT NULL
 );
 
 DROP TABLE IF EXISTS Asignaturas;
 CREATE TABLE IF NOT EXISTS Asignaturas(
-nombre VARCHAR(255) PRIMARY KEY,
-descripcion VARCHAR(255) 
+nombre_asignatura VARCHAR(255),
+descripcion VARCHAR(255),
+profesor varchar(255),
+primary key(nombre_asignatura)
 );
 
-INSERT INTO Alumnos VALUES ('11111111-A', 'Antonio', 'Macias Muñoz', 21, '00000000-Z'),
-('22222222-B', 'Manuel', 'Ramos Adame', 24, '00000000-Z'),
-('33333333-C', 'Gabriel', 'Ramos Suan', 26, '99999999-Y');
-INSERT INTO Profesores VALUES ('00000000-Z', 'Juan', 'García Pérez', 'Servidor'),
-('99999999-Y', 'Jesus', 'Costa Costa', 'Aplicaciones');
-INSERT INTO Asignaturas VALUES ('Servidor', 'Lenguaje del entorno del servidor'),
-('Aplicaciones', 'Aplicaciones para el servidor');
+DROP TABLE IF EXISTS Alumno_Asignatura;
+CREATE TABLE IF NOT EXISTS Alumno_Asignatura(
+dni VARCHAR(9),
+nombre_asignatura VARCHAR(255),
+CONSTRAINT primary key (dni, nombre_asignatura),
+FOREIGN KEY(dni) REFERENCES Alumnos(dni),
+FOREIGN KEY(nombre_asignatura) REFERENCES Asignaturas(nombre_asignatura)
+);
 
-ALTER TABLE Alumnos ADD FOREIGN KEY (profesor) REFERENCES Profesores(dni);
-ALTER TABLE Profesores ADD FOREIGN KEY (asignatura) REFERENCES Asignaturas(nombre);
+select * from Profesores;
+
+
+INSERT INTO Alumnos VALUES 
+('11111111A', 'Antonio', 'Macias Muñoz', 21),
+('22222222B', 'Manuel', 'Ramos Adame', 24),
+('33333333C', 'Gabriel', 'Ramos Suan', 26);
+
+INSERT INTO Asignaturas VALUES 
+('Cliente', 'Lenguaje del entorno del Cliente', 'Juan Antonio Calvo'),
+('Aplicaciones', 'Aplicaciones de PHP','Jesus Isidoro Costas');
+INSERT INTO Alumno_Asignatura VALUES ('11111111A', 'Cliente'),
+('22222222B', 'Cliente'),('22222222B', 'Aplicaciones'),('33333333C', 'Cliente');
 
 /* CONSULTAS DE TABLAS */
-SELECT * FROM Alumnos;
+/*SELECT * FROM Alumnos;
 SELECT * FROM Profesores;
-SELECT * FROM Asignaturas;
+SELECT * FROM Asignaturas;*/
 
-/* CONSULTAS RELACIONADAS */
-SELECT Alumnos.*, Profesores.nombre AS 'NombreProfesor' FROM Alumnos JOIN Profesores WHERE Alumnos.profesor = Profesores.dni;
+/*Los alumnos del profesor Jesus Costa*/
+select * from Alumnos join Alumno_Asignatura on Alumnos.dni=Alumno_Asignatura.dni 
+join Asignaturas on Alumno_Asignatura.nombre_asignatura=Asignaturas.nombre_asignatura
+where profesor='Jesus Isidoro Costas';
